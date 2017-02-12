0def7f6e83899de4caff7271e8ac7a535cdfa2ab
Louis Grignon <louis.grignon@gmail.com>
2017-02-12 01:18:26 +0100

    fixed module names

--- src/main/java/def/express/express_lib_express/package-info.java
+++ src/main/java/def/express/express_lib_express/package-info.java
@@ -1,4 +1,4 @@
 /** (source: Definitely Typed) */
 //@jsweet.lang.Name("~express/lib/express")
-@jsweet.lang.Module("~express/lib/express")
+@jsweet.lang.Module("express")
 package def.express.express_lib_express;
